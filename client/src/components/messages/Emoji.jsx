3b685e30ab59656067b5b28691c4d37fcbf6456b
SiddhantVgaikwad <128611331+SiddhantVgaikwad@users.noreply.github.com>
2024-04-16 20:25:57 +0530

    first commit

---
+++ client/src/components/messages/Emoji.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import Picker from '@emoji-mart/data';
+import data from '@emoji-mart/data'
+
+function Emoji() {
+
+    const [isPickerVisible, setPickerVisible] = useState(false);
+    const [currentEmoji, setCurrentEmoji] = useState(null);
+
+  return (
+    <div>
+    <button onClick={() => setPickerVisible (!isPickerVisible)}>
+        😊
+    </button>
+    <div className={isPickerVisible ? 'd-block': 'd-none'}>
+        <Picker data={data} previewPosition = "none" onEmojiSelect={(e) => {
+            setCurrentEmoji(e.native)
+            setPickerVisible(!isPickerVisible)
+        }}/>
+    </div>
+    </div>
+  )
+}
+
+export default Emoji
---
+++ server/server.js
@@ -0,0 +1,36 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./config/mongoDB.js";
+import { app, server } from "./socket/socket.js";
+
+
+
+const __dirname = path.resolve();
+
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+const PORT = process.env.PORT;
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
+});
